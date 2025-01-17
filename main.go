0ddb6bda482ff6e11d96399f29e5ebfafbbf3adb
jtrahan <justin@trahan.dev>
2025-01-17 09:03:02 -0600

    reqs

--- main.go
+++ main.go
@@ -3,28 +3,36 @@ package main
 import (
 	"embed"
 	"log/slog"
+	"os"
 
 	"github.com/babbage88/infra-db/infra_db"
+	"github.com/joho/godotenv"
+	"github.com/pressly/goose/v3"
 )
 
 //go:embed migrations/*
-var embededMigrations embed.FS
+var embedMigrations embed.FS
 
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		slog.Error("Error loading .env",  slog.String("error", err.Error()))
+		slog.Error("Error loading .env", slog.String("error", err.Error()))
 	}
-	db, err := infra_db.InitializeDbConnectionFromUrl(dbUrl string)
-	
+
+	db, err := infra_db.InitializeDbConnectionFromUrl(os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		slog.Error("Error Initializing db", slog.String("error", err.Error()))
+	}
+
 	slog.Info("Starting migratioms")
 	goose.SetBaseFS(embedMigrations)
 
 	if err := goose.SetDialect("postgres"); err != nil {
-		    panic(err)
+		panic(err)
 	}
 
 	if err := goose.Up(db, "migrations"); err != nil {
-		    panic(err)
-	}	
+		panic(err)
+	}
 }
