30236d4667c6a9901c8cece830f9d49fa10a9034
Justin Trahan <justin@trahan.dev>
2025-05-18 09:46:45 -0500

    set token to be NOT NULL

--- migrations/00041_authtoken_not_null.sql
+++ migrations/00041_authtoken_not_null.sql
@@ -1,9 +1,43 @@
 -- +goose Up
+
 -- +goose StatementBegin
-SELECT 'up SQL query';
+DO $$
+BEGIN
+    RAISE NOTICE 'Starting UP Migration 00040->00041';
+    RAISE NOTICE 'Altering external_auth_tokens.token column to SET NOT NULL';
+END $$;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+DO $$
+BEGIN
+  RAISE NOTICE 'Setting NULL token records to empty string';
+END $$;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+-- Replace NULLs with empty string or a placeholder
+UPDATE public.external_auth_tokens
+SET token = ''
+WHERE token IS NULL;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+ALTER TABLE public.external_auth_tokens
+    ALTER COLUMN token SET NOT NULL;
 -- +goose StatementEnd
 
 -- +goose Down
+
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DO $$
+BEGIN
+    RAISE NOTICE 'Starting DOWN Migration 00041->00040';
+    RAISE NOTICE 'Altering external_auth_tokens.token column to SET NOT NULL';
+END $$;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+ALTER TABLE public.external_auth_tokens
+    ALTER COLUMN token DROP NOT NULL;
 -- +goose StatementEnd
